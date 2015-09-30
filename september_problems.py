cad4ff6c33a774800d66d80adf33294f19d86ac5
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-09-29 21:16:07 -0600

    Array
    Basically you need to find out if the numbers in the array are larger
    then the biggest number in the array.

--- september_problems.py
+++ september_problems.py
@@ -1,10 +1,10 @@
 
-You and your date are trying to get a table at a restaurant.
-The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is
-the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no,
-1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes).
-With the exception that if either of you has style of 2 or less, then the result is 0 (no).
-Otherwise the result is 1 (maybe).
+#You and your date are trying to get a table at a restaurant.
+#The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is
+#the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no,
+#1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes).
+#With the exception that if either of you has style of 2 or less, then the result is 0 (no).
+#Otherwise the result is 1 (maybe).
 
 date_fashion(5, 10) → 2
 date_fashion(5, 2) → 0
@@ -19,3 +19,25 @@ def date_fashion(you, date):
    return 2
   else:
    return 1
+
+
+#Using the Python language, have the function ArrayAdditionI(arr) take 
+#the array of numbers stored in arr and return the string true if any
+#combination of numbers in the array can be added up to equal the largest
+#number in the array, otherwise return the string false. For example: if
+#arr contains [4, 6, 23, 10, 1, 3] the output should return true because
+#4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all
+#the same elements, and may contain negative numbers.
+
+
+
+def ArrayAdditionI(arr):
+
+  # code goes here
+  sort = arr.sort()
+  largest = arr.pop()
+  sumarr = sort.sum()
+  if sumarr > largest:
+    return true
+  else:
+    return false
