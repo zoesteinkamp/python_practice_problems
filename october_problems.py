057d4dab5260ec9a5cf9f9b7b9604b617827d618
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-10-13 21:31:06 -0600

    October problem 2
    Uncompleted

--- october_problems.py
+++ october_problems.py
@@ -21,3 +21,25 @@ def ExOh(index):
 
 ExOh(string) #returns true
 ExOh(string_2) #returns false
+
+
+
+
+#Using the Python language, have the function RunLength(str) take the str parameter being passed
+#and return a compressed version of the string using the Run-length encoding algorithm.
+#This algorithm works by taking the occurrence of each repeating character and outputting that number
+#along with a single character of the repeating sequence. For example: "wwwggopp" would return 3w2g1o2p.
+#The string will not contain any numbers, punctuation, or symbols.
+
+Input = "aabbcde" # should return 2a2b1c1d1e
+
+def RunLength(str):
+    array = str.split(',')
+    print(array)
+    for i in range(0,len(array)):
+        print(array[i]+1)
+
+RunLength(Input)
+
+
+# I did not have the time to finish this. s
